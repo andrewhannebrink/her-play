1286d732ea94ad7756031d5bd6808b47e0c0a842
Andrew Hannebrink <andrewhannebrink@Andrews-MacBook-Pro.local>
2025-08-03 11:13:13 -0500

    repo name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/YOUR_REPO_NAME/', // Replace with your GitHub repo name
+  base: '/her-play/', // Replace with your GitHub repo name
   build: {
     outDir: 'dist',
   }
